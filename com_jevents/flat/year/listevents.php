9d43e87e97cf2a4ebde1ac55284b273600cfefb1
Emanuele Cappato <e.kappato@icloud.com>
2018-01-11 12:25:05 +0100

    Add files via upload

--- com_jevents/flat/year/listevents.php
+++ com_jevents/flat/year/listevents.php
@@ -1,16 +1,16 @@
 <?php defined('_JEXEC') or die('Restricted access'); ?>
 
-<div class="wrapper jevent-yearview">
+<div class="wrapper jevent-wapper jevent-yearview">
 	<div class="container">
 		<div class="row">
 
 			<div class="col-12">
 				<?php //$this->_header(); ?>
 			</div>
-			<div class="col-3 sidebar-alt">
+			<div class="col-12 col-sm-12 col-md-12 col-lg-3 sidebar-alt">
 				<?php $this->_showNavTableBar(); ?>
 			</div>
-			<div class="col-9">
+			<div class="col-12 col-sm-12 col-md-12 col-lg-9" id="jevents_body">
 				<?php $params = JComponentHelper::getParams(JEV_COM_COMPONENT); ?>
 				<?php if ($params->get("row","")!=""){ ?>
 					<?php echo $this->loadTemplate("body"); ?>
@@ -20,7 +20,7 @@
 			</div>
 			<div class="col-12">
 				<?php $this->_viewNavAdminPanel(); ?>
-				<?php $this->_footer(); ?>
+				<?php //$this->_footer(); ?>
 			</div>
 		</div>
 	</div>
